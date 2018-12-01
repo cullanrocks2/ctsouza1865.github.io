e6bc953baeb23c7a5a9e2773623a5e5aaca730af
cullanrocks2 <43248066+cullanrocks2@users.noreply.github.com>
2018-11-30 23:55:10 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.font = "40px Georgia";
+ctx.strokeStyle = "white";
 ctx.strokeText("Side Projects", 40, 50);
+
+
 
 
  
